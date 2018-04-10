a4d120fbbea545e6cacc75c87e8916c37df24792
Chad A. Woitas <woitas.chad@gmail.com>
2018-04-10 08:22:35 -0600

    init commit
    Commiting core node files

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const fs = require('fs');
+
+
+fs.readFile('index.html',(err,html) => {
+	if(err){
+		throw err;
+	}
+
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.write(html);
+  res.end('Hello World\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+});
